2fe97034af93e42b2eec96b5b423481704952409
jixatos <j.gody2004@gmail.com>
2023-11-06 09:27:28 -0300

    Criação do inputCpf

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ def inputTelefone():
     except Exception as e:
         print("Algo ocorreu errado", e)
         return None
+
+def inputCpf():
+    try:
+        cpf = input("CPF: ")
+        car = ".-/ "
+
+        for i in range (0, len(car)):
+            cpf = cpf.replace(car[i], "")
+        cpf = int(cpf)
+
+        return cpf
+    except ValueError as e:
+        print("Insira um valort válido", e)
+        return "ValueError"
+    except Exception as e:
+        print("Algo ocorreu errado", e)
+        return None
