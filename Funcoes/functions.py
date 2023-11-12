ee64767d79d8fbeaf6f51594d76a549c84304d6d
jixatos <j.gody2004@gmail.com>
2023-11-12 00:10:52 -0300

    Verificação de CEP por API, função de cadastro e login

--- Funcoes/functions.py
+++ Funcoes/functions.py
@@ -1,4 +1,6 @@
+from DataBase.crud import getConnection, closeConnection, insert, select, update, delete
 import getpass
+import requests
 
 def inputTelefone():
     try:
@@ -66,3 +68,78 @@ def inputPassword():
         print("Erro na entrada na Senha: ", e)
         return None
 
+def inputCep():
+    try:
+        cep = input("CEP: ")
+
+        # (REGEX)
+        car = ".-/ "
+        for i in range(0, len(car)):
+            cep = cep.replace(car[i], "")
+        cep = int(cep)
+
+        return cep
+    except Exception as e:
+        print("Erro de entrada no CPF: ",e)
+
+def GETViaCep(cep):
+    try:
+        url = f'https://viacep.com.br/ws/{cep}/json/'
+        requisicao = requests.get(url)
+        dic = requisicao.json()
+        return dic
+    except Exception as e:
+        print("Erro no GETCep: ",e)
+
+def verCep(dic):
+    try:
+        cep = dic['cep']
+        if cep is not None:
+            return dic
+    except KeyError:
+        print("O CEP foi digitado errado ou é inexistente")
+    except Exception as e:
+        print("Erro com o Objeto JSON recuperado pelo ViaCEP: ",e)
+
+def cadastro(usuario):
+    try:
+        rows_user = [row for row in usuario.keys()]
+        str_rows_user = ",".join(rows_user)
+
+        email = usuario['email']
+        nome = usuario['nome']
+        telefone = usuario['telefone']
+        senha = usuario['senha']
+
+        values_user = f'{email},{nome},{telefone},{senha}'
+
+        endereco = usuario['endereco']
+        rows_end = [rows for rows in endereco.keys()]
+        str_rows_end = ",".join(rows_end)
+
+        cep = endereco['cep']
+        rua = endereco['rua']
+        numero = int(endereco['numero'])
+        complemento = endereco['complemento']
+        estado = endereco['estado']
+        cidade = endereco['cidade']
+
+        values_end = f'{cep},{rua},{numero},{complemento},{estado},{cidade}'
+
+        #Inserção na tabela de endereco
+        insert('endereco',str_rows_end,values_end)
+
+        #inserção na tabela de cliente
+        insert('cliente',str_rows_user,values_user)
+    except Exception as e:
+        print("Erro no cadastro do usuário: ",e)
+
+def login(email, senha):
+    try:
+        tuple = select('cliente', 'email', email)
+        if email == tuple[0] and senha == tuple[3]:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("Erro no login: ", e)
